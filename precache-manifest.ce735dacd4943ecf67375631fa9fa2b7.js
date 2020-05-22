853261738875e6e3b011fdc1d961d895c3b438a9
Deeds <romaz20@ukr.net>
2020-05-22 15:53:59 +0300

    Updates

---
+++ precache-manifest.ce735dacd4943ecf67375631fa9fa2b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9f0b4ab5a0851dc4d92e8d99850fae",
+    "url": "/React-Simple-SPI/index.html"
+  },
+  {
+    "revision": "25444b731de10937759e",
+    "url": "/React-Simple-SPI/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "dafd7f9a606ce94b16f3",
+    "url": "/React-Simple-SPI/static/css/main.9d27de8c.chunk.css"
+  },
+  {
+    "revision": "25444b731de10937759e",
+    "url": "/React-Simple-SPI/static/js/2.915eb5f5.chunk.js"
+  },
+  {
+    "revision": "ddfc0096585954e7cc3b2c8645007c51",
+    "url": "/React-Simple-SPI/static/js/2.915eb5f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafd7f9a606ce94b16f3",
+    "url": "/React-Simple-SPI/static/js/main.4561a1a5.chunk.js"
+  },
+  {
+    "revision": "34b9c7cafe392a60032d",
+    "url": "/React-Simple-SPI/static/js/runtime-main.d7d5ee9d.js"
+  }
+]);
